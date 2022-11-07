42eb1a21481e1a361fd06640b15f21695f8b1d98
johanuribe03 <johan.uribe@gmail.com>
2022-11-07 11:38:25 +0000

    creating functions

--- hangman.py
+++ hangman.py
@@ -1,3 +1,18 @@
 import random
 from words import words
-print(words)
+import string
+# print(words)
+
+def get_valid_word(words):
+    word = random.choice(words) #randomly chooses a word from the list
+    while '_' in word or ' ' in word: # while - or ' ' is in this word keep choosing the word
+        word = random.choice(words)
+
+    return word
+
+# which letters we guess and which letters we correctly guessed
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(words) #variable that saves letters in the word
+    alphabet - set(string.ascii_uppercase) #import list of uppercase characters
+    used_letters = set() # to track what the user has guessed
